606f32cedf7a6d700445080664a693d040922001
Diana Tran <trandianat@gmail.com>
2020-12-18 13:35:14 -0500

    Testing deployed Lambda

--- lambda/main.ts
+++ lambda/main.ts
@@ -1,3 +1,5 @@
-exports.handler = () => {
-    return { response: 'Test!' };
+export const handler = async (event: any = {}): Promise<any> => {
+    console.log('Hello World!');
+    const response = JSON.stringify(event, null, 2);
+    return response;
 };
