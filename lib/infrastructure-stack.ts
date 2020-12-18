c5ee7ff8530bbd43709fa51a04518aca3646938d
Diana Tran <trandianat@gmail.com>
2020-12-18 12:48:44 -0500

    Updated website

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
 
@@ -6,15 +7,22 @@ export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const myBucket = new s3.Bucket(this, 'trandianat-cdk', {
+    const bucket = new s3.Bucket(this, 'trandianat-cdk', {
       publicReadAccess: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       websiteIndexDocument: 'index.html',
     });
 
     const deployment = new s3Deployment.BucketDeployment(this, 'deployStaticWebsite', {
+      destinationBucket: bucket,
       sources: [s3Deployment.Source.asset('./src')],
-      destinationBucket: myBucket
+    });
+
+    const handler = new lambda.Function(this, 'LambdaHandler', {
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'main.handler',
+      memorySize: 1024,
+      runtime: lambda.Runtime.NODEJS_12_X,
     });
   }
 }
